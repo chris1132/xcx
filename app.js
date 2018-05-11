a1f94ea16921ea9be431ed58d759948fa4964574
chris1132 <793224504@qq.com>
2018-05-11 16:59:01 +0800

    微信登录验证

--- app.js
+++ app.js
@@ -1,34 +1,84 @@
 //app.js
 App({
   onLaunch: function () {
-    // 展示本地存储能力
-    var logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
+    // // 展示本地存储能力
+    // var logs = wx.getStorageSync('logs') || []
+    // logs.unshift(Date.now())
+    // wx.setStorageSync('logs', logs)
+    var that = this;
 
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
-    // 获取用户信息
+    // 获取用户设置信息
     wx.getSetting({
       success: res => {
         if (res.authSetting['scope.userInfo']) {
-          // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
-          wx.getUserInfo({
-            success: res => {
-              // 可以将 res 发送给后台解码出 unionId
-              this.globalData.userInfo = res.userInfo
-
-              // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-              // 所以此处加入 callback 以防止这种情况
-              if (this.userInfoReadyCallback) {
-                this.userInfoReadyCallback(res)
-              }
+          that.checkSession()
+        } else {
+          wx.authorize({
+            scope: 'scope.userInfo',
+            success: function () {
+              that.wxLogin()
+              that.getuserInfo()
             }
           })
+        }
+      }
+    })
+  },
+  checkSession:function(){
+    var that = this;
+    wx.checkSession({
+      success: function () {
+        var sessionId = wx.getStorageSync("thirdSessionId")
+        wx.request({
+          url: "http://www.dzgj.com/springboot/thirdSessionIdCheck?thirdSessionId=" + sessionId,
+          method: "POST",
+          header: { "Content-Type": "application/json" },
+          success: res => {
+              //第三方session未过期
+              //TODO 
+            if (res.data==1) {
+              
+            } else {
+              //验证不通过
+              that.wxLogin()
+            }
+          }
+        })
+      },
+      fail: function () {
+        that.wxLogin();
+      }
+    })
+  },
+  wxLogin:function(){
+    wx.login({
+      success: res => {
+        wx.request({
+          url: 'http://www.dzgj.com/springboot/codeCheck?code=' + res.code,
+          method: 'POST',
+          header: {
+            "Content-Type": "application/json"
+          },
+          success: res => {
+            wx.setStorageSync("thirdSessionId", res.data)
+          }
+        })
+        // 发送 res.code 到后台换取 openId, sessionKey, unionId
+      }
+    })
+  },
+  getuserInfo:function(){
+    var that = this
+    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+    wx.getUserInfo({
+      success: res => {
+        // 可以将 res 发送给后台解码出 unionId
+        that.globalData.userInfo = res.userInfo
+
+        // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+        // 所以此处加入 callback 以防止这种情况
+        if (that.userInfoReadyCallback) {
+          that.userInfoReadyCallback(res)
         }
       }
     })
--- pages/suggestion/suggestion.js
+++ pages/suggestion/suggestion.js
@@ -21,32 +21,16 @@ Page({
       qqmapsdk = new QQMapWX({
         key: 'VOQBZ-NCWKX-YGO4J-T6QIX-Z6LCV-WMFUD'
       });
-      suggestFail = function (res) {
-        console.log(res)
-      };
-      //调用suggest接口成功函数
-      suggestSuccess = function (res) {
-        that.setData({
-          dataAry: res.data
-        });
-      };
-      var regeFail = function (res) {
-        console.log(res)
-      };
-      var regeSuccess = function (res) {
-        that.setData({
-          latitude: res.latitude
-        });
-        that.setData({
-          longitude: res.longitude
-        });
-      };
       wx.getLocation({
         type:'wgs84',
-        success: regeSuccess,
-        fail: regeFail
+        success: res=>{
+          that.setData({
+            latitude: res.latitude,
+            longitude: res.longitude
+          });
+        },
+        fail: res=>{console.log(res)}
       })
-
     },
     //input框输入监听事件
     bindKeyInput: function (e) {
@@ -62,8 +46,12 @@ Page({
         keyword: e.detail.value,
         region: '嘉兴',
         policy: 1,
-        success: suggestSuccess,
-        fail: suggestFail
+        success: res => {
+          that.setData({
+            dataAry: res.data
+          });
+        },
+        fail: res => { console.log(res)}
       })
     },
     //地址提示栏选择
